829ca6a5cc66de0b270d053fb90133324cbeb0c5
Hovo Petrosyan <hovpetrosyan.github@gmail.com>
2026-02-14 11:14:25 +0400

    feat: pages creation with routing

---
+++ packages/ui/pages/board/[taskId].vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen bg-brand flex items-center justify-center">
+    <h1 class="text-white text-3xl font-bold">
+      Board ID: {{ $route.params.taskId }}
+    </h1>
+  </div>
+</template>
--- packages/ui/pages/board/index.vue
+++ packages/ui/pages/board/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="h-screen bg-brand flex items-center justify-center">
-    <h1 class="text-white text-3xl font-bold">About Page</h1>
+    <h1 class="text-white text-3xl font-bold">Board</h1>
   </div>
 </template>
