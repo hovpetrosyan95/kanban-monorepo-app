f5c54e9a5fde4bdfb6783f12092d0b42020376a5
Hovo Petrosyan <hovpetrosyan.github@gmail.com>
2026-02-14 13:16:08 +0400

    feat: navbar created, shared

---
+++ packages/ui/components/NavLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink
+    :to="to"
+    class="group flex h-12 w-12 items-center justify-center rounded-xl text-text-muted transition-all duration-200 hover:bg-brand-light hover:text-brand [&.router-link-active]:bg-brand-light [&.router-link-active]:text-brand [&.router-link-active]:font-bold lg:[&.router-link-active]:rounded-lg lg:[&.router-link-active]:shadow-sm"
+  >
+    <slot />
+  </NuxtLink>
+</template>
+
+<script setup>
+defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+});
+</script>
---
+++ packages/ui/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen bg-gray-50 flex flex-col lg:flex-row">
+    <main class="flex-1 pb-16 lg:pb-0 lg:pl-20">
+      <slot />
+    </main>
+
+    <Navbar />
+  </div>
+</template>
