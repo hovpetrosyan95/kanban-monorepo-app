535cf44957b2179be6ea9d7a99a78b5905a32222
Hovo Petrosyan <hovpetrosyan.github@gmail.com>
2026-02-18 15:00:02 +0400

    chore: capacitor config

--- packages/mobile/nuxt.config.ts
+++ packages/mobile/nuxt.config.ts
@@ -1,19 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // 1. Inherit everything (Tailwind plugin, CSS, Components, Pages)
-  extends: ["@kanban/ui"],
-
-  // 2. Platform specific overrides
   ssr: false,
 
+  nitro: {
+    preset: "static",
+    prerender: {
+      crawlLinks: true,
+      routes: ["/"], // Force generate index.html
+    },
+  },
   app: {
     baseURL: "./",
-    buildAssetsDir: "assets",
+    buildAssetsDir: "_nuxt/",
   },
 
-  nitro: {
-    output: { publicDir: "dist" },
-  },
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  extends: ["../ui"],
 });
